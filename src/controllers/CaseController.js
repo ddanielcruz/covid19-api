2d3306c9bb00f547217a10aceef6d5120de46977
Daniel Cunha <danielcunha54@gmail.com>
2020-04-08 15:36:00 -0300

    Fetch cases by day

--- src/controllers/CaseController.js
+++ src/controllers/CaseController.js
@@ -1,9 +1,38 @@
+const moment = require("moment");
+
 const Extraction = require("../models/Extraction");
 
+const fetchCases = async (date) => {
+  const extractions = await Extraction.find({
+    createdAt: {
+      $gte: date.startOf("day").toDate(),
+      $lte: moment(date).endOf("day").toDate(),
+    },
+  }).populate("place");
+
+  // Remove repeated extractions and keep only the most recent ones
+  const mappedExtractions = extractions.reverse().reduce((acc, curr) => {
+    if (!acc[curr.place.name]) {
+      acc[curr.place.name] = curr;
+    }
+
+    return acc;
+  }, {});
+
+  return Object.values(mappedExtractions);
+};
+
 exports.show = async (request, response) => {
-  return response.json({ ok: true });
+  const { parsedDate } = request.params;
+  const cases = await fetchCases(parsedDate);
+
+  response.header("X-Total-Count", cases.length);
+  return response.json(cases);
 };
 
 exports.today = async (request, response) => {
-  return response.json({ ok: true });
+  const cases = await fetchCases(moment());
+
+  response.header("X-Total-Count", cases.length);
+  return response.json(cases);
 };
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ const express = require("express");
 const PlaceController = require("./controllers/PlaceController");
 const CaseController = require("./controllers/CaseController");
 const ExtractionController = require("./controllers/ExtractionController");
+
 const validateId = require("./middlewares/validateId");
+const validateDate = require("./middlewares/validateDate");
 
 const routes = express.Router();
 
@@ -11,7 +13,7 @@ routes.get("/places", PlaceController.index);
 routes.get("/places/:id", validateId, PlaceController.show);
 
 routes.get("/cases/today", CaseController.today);
-routes.get("/cases/:date", CaseController.show);
+routes.get("/cases/:date", validateDate(), CaseController.show);
 
 routes.get("/extractions", ExtractionController.index);
 routes.get("/extractions/place/:id", validateId, ExtractionController.byPlace);
