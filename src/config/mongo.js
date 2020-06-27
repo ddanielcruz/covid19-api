c3c66fda3524b042fec779fe7135546d0463d305
Daniel Cunha <danielcunha54@gmail.com>
2020-06-27 14:03:30 -0300

    refactor: Clean up the project

--- src/config/mongo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-exports.config = () => {
-  return mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-};
