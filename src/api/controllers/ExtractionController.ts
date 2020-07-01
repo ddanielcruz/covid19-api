58c3e5cf7caac2220ba1e9f5b30567590f52aaa0
Daniel Cunha <danielcunha54@gmail.com>
2020-07-01 17:58:05 -0300

    feat: Add endpoint to fetch some place extractions

--- src/api/controllers/ExtractionController.ts
+++ src/api/controllers/ExtractionController.ts
@@ -4,16 +4,12 @@ import ExtractionService from '@services/ExtractionService';
 class ExtractionController {
   private service: ExtractionService = new ExtractionService();
 
-  byPlace = async (request: Request, response: Response): Promise<Response> => {
+  findByPlace = async (
+    request: Request,
+    response: Response
+  ): Promise<Response> => {
     const { id } = request.params;
     const extractions = await this.service.findByPlace(id);
-
-    response.header('X-Total-Count', extractions.length.toString());
-    return response.json(extractions);
-  };
-
-  today = async (request: Request, response: Response): Promise<Response> => {
-    const extractions = await this.service.findByDate(new Date());
 
     response.header('X-Total-Count', extractions.length.toString());
     return response.json(extractions);
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -7,6 +7,11 @@ const routes = Router();
 // Places
 routes.get('/places', controllers.Place.index);
 routes.get('/places/:id', hasValidId, controllers.Place.show);
+routes.get(
+  '/places/:id/extractions',
+  hasValidId,
+  controllers.Extraction.findByPlace
+);
 
 // Cases
 routes.get('/cases/today', controllers.Case.today);
